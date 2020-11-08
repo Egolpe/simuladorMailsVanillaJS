4587036302d6e6176e12a78f66c2205516eee405
Enrique Golpe <enriquegolpe@hotmail.com>
2020-11-08 21:17:35 +0100

    first commit

---
+++ www/app.js
@@ -0,0 +1,127 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar')
+const btnReset = document.querySelector('#resetBtn')
+const formulario = document.querySelector('#enviar-mail')
+
+// Variables para campos
+const email = document.querySelector('#email')
+const asunto = document.querySelector('#asunto')
+const mensaje = document.querySelector('#mensaje')
+
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            
+
+eventListeners()
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', iniciarApp)
+
+    //Campos del formulario
+    email.addEventListener('blur', validarFormulario)
+    asunto.addEventListener('blur', validarFormulario)
+    mensaje.addEventListener('blur', validarFormulario)
+
+    
+    //Enviar email
+    formulario.addEventListener('submit', enviarEmail)
+    //reiniciar formulario
+    btnReset.addEventListener('click', resetearFormulario)
+}
+
+
+// Funciones
+
+    function iniciarApp(){
+        btnEnviar.disabled = false
+        btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50')
+    }
+
+    //Validar formulario
+    function validarFormulario(e) {
+
+
+        if(e.target.value.length > 0){
+
+            // elimina los errores 
+            const error = document.querySelector('p.error')
+            if(error){error.remove}
+            
+
+            e.target.classList.remove('border', 'border-red-500')
+            e.target.classList.add('border', 'border-green-500')
+        } else {
+            e.target.classList.remove('border', 'border-green-500')
+            e.target.classList.add('border', 'border-red-500')
+            mostrarError('Todos los campos son obligatorios')
+        }
+
+        if(e.target.type === 'email'){
+            
+            if(er.test(e.target.value) ){
+                const error = document.querySelector('p.error')
+                if(error){ error.remove()}
+               
+                
+                e.target.classList.remove('border', 'border-red-500')
+                e.target.classList.add('border', 'border-green-500')
+            }else {
+                e.target.classList.remove('border', 'border-green-500')
+                e.target.classList.add('border', 'border-red-500')
+
+                mostrarError('Email no válido')
+            }
+        }
+
+        if ( er.test (email.value) !== '' && asunto.value !== '' && mensaje.value !== '' ){
+            console.log('pasaste la validación')
+        }
+        
+}    
+
+    function mostrarError(mensaje){
+        const mensajeError = document.createElement('p')
+        mensajeError.textContent = mensaje
+        mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center','error')
+
+        const errores = document.querySelectorAll('.error')
+        if(errores.length === 0){
+            formulario.appendChild(mensajeError)
+        }
+
+}
+
+function enviarEmail(e){
+    e.preventDefault()
+
+    //mostrar el spiner
+    const spinner = document.querySelector('#spinner')
+    spinner.style.display = 'flex'
+
+
+    // Despues de 5 segundos ocultar el spinner y mostrar el mensaje
+    setTimeout(() => {
+        spinner.style.display = 'none'
+
+        // mensaje de envio correcto 
+        const parrafo = document.createElement('p')
+        parrafo.textContent = 'El mensaje se envió correctamente'
+        parrafo.classList.add('text-center', 'my-10', 'p-2', 'bg-green-500', 'text-white', 'font-bold', 'uppercase')
+
+        //Inserta el párrafo antes del spinner
+        formulario.insertBefore(parrafo, spinner)
+
+        setTimeout(() => {
+            parrafo.remove()
+            resetearFormulario()
+           
+        },5000)
+
+    },3000)
+}
+
+// Función que resetea el formulario 
+function resetearFormulario(e){
+    e.preventDefault()
+    formulario.reset()
+   
+}
+
